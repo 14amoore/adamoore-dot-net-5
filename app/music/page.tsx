0bb925fc3df2dc01bf662e1dec96a914bfd40755
Adam Moore <adamoore@gmail.com>
2023-10-27 14:51:46 -0400

    adding padding and color to links

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -29,21 +29,16 @@ const getTopArtists = async (token: string) => {
 export default async function Spotify() {
     const data = await getData()
     const topArtistItem = data.items.map((artist: {id: string; name: string; uri: string}) => 
-        <li key={artist.id}><Link href={artist.uri}>{artist.name}</Link></li>
+        <li className='mb-1' key={artist.id}><Link href={artist.uri}>{artist.name}</Link></li>
     )
     return (
         <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-10">
             <div className="z-10 max-w-5xl w-full font-mono text-lg ">
-                <div>
-                    <h1>These are my top five Spotify artists:</h1>
-                </div>
-                <div>
+                    <h1 className='mb-3'>These are my top five Spotify artists:</h1>
                     <ul>{topArtistItem}</ul>
-                </div>
-                <div>
                     <h1><Link className="text-fuchsia-500 underline" href='/'>Back</Link></h1>
                 </div>
-            </div>
+           
         </main>
     )
 }
