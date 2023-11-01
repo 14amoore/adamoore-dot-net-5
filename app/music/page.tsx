0eadba771c64065309fb5dfacdaba385bf691fa9
Adam Moore <adamoore@gmail.com>
2023-11-01 13:15:38 -0400

    ready for review

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -1,32 +1,11 @@
+import { SPOTIFY_DATA_INPUT } from "@/utils/constants";
 import { getS3Data } from "@/utils/get-s3-data"
-import Link from 'next/link';
-
-const input = {
-  Bucket: 'amoore-spotify-bucket',
-  Key: 'tokens.json'
-}
-
-const getTopArtists = async (token: string) => {
-    const top_artists_url =
-  'https://api.spotify.com/v1/me/top/artists?time_range=short_term&limit=5';
-    const headers = {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
-    };
-  
-    const response = await fetch(top_artists_url, { headers: headers });
-    
-    
-    
-    const artists = await response.json();
-   
-    
-    return artists;
-  };
+import { getTopArtists } from '@/utils/getTopArtists'
+import Link from "next/link";
+import StyledLink from "@/components/StyledLink";
  
  async function getData () {
-    const {access_token} = await getS3Data(input)
+    const {access_token} = await getS3Data(SPOTIFY_DATA_INPUT)
     const topArtists = await getTopArtists(access_token)
     return topArtists
 }
@@ -37,13 +16,10 @@ export default async function Spotify() {
         <li className='mb-1' key={artist.id}><Link href={artist.uri}>{artist.name}</Link></li>
     )
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-10">
-            <div className="z-10 max-w-5xl w-full font-mono text-lg ">
-                    <h1 className='mb-3'>These are my top five Spotify artists:</h1>
-                    <ul>{topArtistItem}</ul>
-                    <h1><Link className="text-fuchsia-500 underline" href='/'>Back</Link></h1>
-                </div>
-           
-        </main>
+      <div className="z-10 max-w-5xl w-full font-mono text-lg ">
+        <h1 className='mb-3'>These are my top five Spotify artists:</h1>
+        <ul>{topArtistItem}</ul>
+        <StyledLink route={'/'} linkType={'Back'} routeText={'Back'}/>
+      </div>   
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,29 @@
+import { WEATHER_DATA_INPUT } from "@/utils/constants"
 import { getS3Data } from "@/utils/get-s3-data"
 import  cloudCover from '@/utils/cloud-cover'
-import Link from "next/link"
+import StyledLink from "@/components/StyledLink"
+import DisplayColorElement from "@/components/DisplayColorElement"
 
 export const dynamic = 'force-dynamic'
 
-const input = {
-  Bucket: 'amoore-nyc-weather',
-  Key: 'weather.json'
-}
+
+
 
 export default async function Home() {
-  const weather = await getS3Data(input);
-  const {sky_hex_color, current_temp, cloudcover_percentage} = weather
+  const weather = await getS3Data(WEATHER_DATA_INPUT);
+  const {sky_hex_color, current_temp, cloudcover_percentage, day_or_night} = weather
   const clouds = cloudCover(cloudcover_percentage)
-  const textColor = {
-    color: sky_hex_color
-  }
-  
-    
+       
   return (
-    <main className="flex flex-col z-10 max-w-5xl w-full font-mono text-lg justify-between sm:p-24 p-10 min-h-screen ">
       <div className="flex flex-col gap-y-2">
        <h1 className="mb-2">Hi!</h1>
        <h2>My name is Adam Moore</h2>
        <h3>I&apos;m a Software Engineer and Creative Techologist, with an MFA from Parsons School of Design.</h3>
        <h3>I&apos;m based in Brooklyn where it is currently {current_temp} degrees fahrenheit, with {clouds} skies.</h3>
-       <h3>The sky is pretty close to hex color <span style={textColor}>{sky_hex_color}</span>.</h3>
+       <DisplayColorElement isDay={day_or_night} color={sky_hex_color} />
+       <h3>If you&apos;d like to know more about how this app works please <StyledLink route={'/more-info'} linkType={'internal'} routeText={'click here'} />.</h3>
        <h3>This is my portfolio website. It is, and most likely always will be, a work in progress</h3>
-       <h3>For now take a gander at <Link className="text-fuchsia-500 underline" href='/music'>my top five Artists on Spotify.</Link></h3>
+       <h3>For now take a gander at <StyledLink route={'/music'} linkType={'internal'} routeText={'my top five Spotify artists'} />.</h3>
       </div>
-    </main>
   )
 }
