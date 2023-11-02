98181b30c64ecba4a368e323b1b1e58437956fe9
Adam Moore <adamoore@gmail.com>
2023-11-02 10:52:13 -0400

    fixed typo

--- components/DisplayColorElement.tsx
+++ components/DisplayColorElement.tsx
@@ -4,5 +4,5 @@ export default function DisplayColorElement({isDay, color}: {isDay: number; colo
     if (dayFlag) {
     return <h3>The sky is pretty close to hex color <span style={{color: color}}>{color}</span>.</h3>
     }
-    return <h3>It&apos;s night right now. If you come back during the day you&apos;ll see an the color of the sky here in NY as an approximate hex value.</h3>
+    return <h3>It&apos;s night right now. If you come back during the day you&apos;ll see the color of the sky here in NY as an approximate hex value.</h3>
 }
