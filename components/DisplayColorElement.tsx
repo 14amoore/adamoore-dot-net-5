51f933524b9567a386ddd4bb267707cb750a2681
Adam Moore <adamoore@gmail.com>
2023-11-07 13:12:08 -0500

    adding underline

--- components/DisplayColorElement.tsx
+++ components/DisplayColorElement.tsx
@@ -10,7 +10,7 @@ export default function DisplayColorElement({isDay, color}: DisplayColorElementP
     const dayFlag = !!isDay  
    
     if (dayFlag) {
-    return <h3>The sky is pretty close to hex color <ToolTip skyColor={color}><span style={{color: color}}>{color}</span></ToolTip>.</h3>
+    return <h3>The sky is pretty close to hex color <ToolTip skyColor={color}><span className="underline" style={{color: color}}>{color}</span></ToolTip>.</h3>
     }
     return <h3>It&apos;s night right now. If you come back during the day you&apos;ll see the color of the sky here in NY as an approximate hex value.</h3>
 }
