f1d823701acba01bc8daaba34421f26dc0ce4621
Adam Moore <adamoore@gmail.com>
2023-11-16 13:54:34 -0500

    adding pointer

--- components/ToolTip.tsx
+++ components/ToolTip.tsx
@@ -32,7 +32,7 @@ export default function ToolTip({skyColor, skylineUrl}: ToolTipProps) {
          <span  
             onClick={isVisible ? hideTooltip : showTooltip}
             onMouseEnter={showTooltip}
-            onMouseLeave={hideTooltip} className='underline' style={{color: skyColor}}>{skyColor}</span>
+            onMouseLeave={hideTooltip} className='underline cursor-pointer' style={{color: skyColor}}>{skyColor}</span>
             <div style={{background: skyColor, opacity: isVisible ? 100 : 0}} className='absolute w-72 h-32 bottom-[100%]  transition-opacity duration-500 left-[100%] translate-x-[-50%] mb-3 p-2 z-50 rounded-lg'>
                 <div style={{backgroundImage: `url(${skylineUrl})`, backgroundSize: 'cover'}} className='w-full h-full rounded-lg'></div>
             </div>
