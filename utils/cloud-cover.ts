54ff218ad97ab90a1be78eaff9609db6ac3ec8e0
Adam Moore <adamoore@gmail.com>
2023-11-21 14:28:46 -0500

    updated function

--- utils/cloud-cover.ts
+++ utils/cloud-cover.ts
@@ -6,11 +6,11 @@ export default function cloudCover(cover: Cover): CloudCoverTerms {
     let skyCondition: CloudCoverTerms;
     if(cover <= 12.5 ) {
         skyCondition = 'clear'
-    } else if(cover > 12.5 && cover <=37.5) {
+    } else if(cover <=37.5) {
         skyCondition = 'mostly clear'
-    } else if (cover > 37.5 && cover <= 62.5) {
+    } else if (cover <= 62.5) {
         skyCondition = 'partly cloudy'
-    } else if (cover > 62.5 && cover < 87.5) {
+    } else if (cover < 87.5) {
         skyCondition = 'mostly cloudy'
     } else {
         skyCondition = 'cloudy'
