6a58a8a0b40d2b86e2e014a2e01f9cf8ca02cca6
Adam Moore <adamoore@gmail.com>
2024-11-01 18:37:24 -0400

    more issues

--- app/api/get-s3-data/route.ts
+++ app/api/get-s3-data/route.ts
@@ -3,7 +3,9 @@ import { NextResponse } from 'next/server';
 import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
 import { fromEnv } from '@aws-sdk/credential-provider-env';
 import { Readable } from 'stream';
-import { WEATHER_DATA_INPUT } from '@/utils/constants'; // Adjust this import path as necessary
+import { WEATHER_DATA_INPUT } from '@/utils/constants';
+
+export const dynamic = 'force-dynamic';
 
 const client = new S3Client({ region: 'us-east-1', credentials: fromEnv() });
 
@@ -18,6 +20,14 @@ const streamToString = async (stream: Readable): Promise<string> => {
 };
 
 export async function GET() {
+  // Define headers to prevent caching
+  const headers = new Headers({
+    'Cache-Control': 'no-store, max-age=0',
+    'Pragma': 'no-cache',
+    'Expires': '0',
+    'Content-Type': 'application/json',
+  });
+
   try {
     const command = new GetObjectCommand(WEATHER_DATA_INPUT);
     const { Body } = await client.send(command);
@@ -25,11 +35,6 @@ export async function GET() {
     if (Body instanceof Readable) {
       const data = await streamToString(Body);
       const jsonData = JSON.parse(data);
-
-      // Set headers to prevent caching
-      const headers = new Headers({
-        'Cache-Control': 'no-store, max-age=0',
-      });
 
       // Return data with type detection for structured data
       if ('current_temp' in jsonData) {
@@ -41,10 +46,10 @@ export async function GET() {
         return NextResponse.json({ error: 'Unknown data type' }, { status: 400, headers });
       }
     } else {
-      return NextResponse.json({ error: 'No data found in S3' }, { status: 404 });
+      return NextResponse.json({ error: 'No data found in S3' }, { status: 404, headers });
     }
   } catch (error) {
     console.error('Error fetching data from S3:', error);
-    return NextResponse.json({ error: 'Failed to fetch data from S3' }, { status: 500 });
+    return NextResponse.json({ error: 'Failed to fetch data from S3' }, { status: 500, headers });
   }
 }
