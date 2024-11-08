35e64166d9e91a91b666e7f9660e4994a242a792
Adam Moore <adamoore@gmail.com>
2024-11-08 11:31:51 -0500

    updated readme and styling

--- components/StyledLink.tsx
+++ components/StyledLink.tsx
@@ -1,17 +1,24 @@
-import Link from "next/link";
+import Link from 'next/link';
 
-type LinkType = "internal" | "external" | "Back"
+type LinkType = 'internal' | 'external' | 'Back';
 
 interface LinkProps {
-   route: string,
-   linkType: LinkType,
-   routeText: string
+  route: string;
+  linkType: LinkType;
+  routeText: string;
 }
 
-export default function StyledLink({route, linkType, routeText}: LinkProps) {
-        if (linkType === 'external') {
-           return <Link href={route} className="text-green-500 underline">{routeText}</Link>
-        }
-       return <Link className="text-fuchsia-300 underline" href={route}>{routeText}</Link>
-    
+export default function StyledLink({ route, linkType, routeText }: LinkProps) {
+  if (linkType === 'external') {
+    return (
+      <Link href={route} className="text-green-500 underline font-black">
+        {routeText}
+      </Link>
+    );
+  }
+  return (
+    <Link className="text-fuchsia-300 font-black underline" href={route}>
+      {routeText}
+    </Link>
+  );
 }
